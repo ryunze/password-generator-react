87cb480c586479ba1c1a42efefeea3d9633a21d3
ryunze <ryunzeroeight08@gmail.com>
2025-07-29 18:19:26 +0700

    add lofic

--- src/Components/ContentComponent.jsx
+++ src/Components/ContentComponent.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 export default function ContentComponent() {
 
+    const [password, setPassword] = useState(); 
+    const [passwordLength, setPasswordLength] = useState(8)
+    
+    const generatePassword = function (length = 8) {
+        let result = '';    
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charsLength = characters.length;
+
+        for (let i = 0; i < charsLength; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charsLength));
+        }
+
+        return result.substring(0, length);
+    }
+    
     const handleGeneratePassword = function() {
-        alert("Generated")
+        let generatedPassword = generatePassword(passwordLength)
+        setPassword(generatedPassword)
+    }   
+
+    const handlePasswordLength = function (event) {
+        setPasswordLength(event.target.value)
+        setPassword(generatePassword(passwordLength))
     }
 
     return (
@@ -12,10 +35,16 @@ export default function ContentComponent() {
                     <p>Generator sederhana untuk mempermudah dan menjaga <br/> keamanan akun online kamu.</p>
                 </div>
                 <div className="w-1/2 bg-white p-6 rounded-xl shadow">
+
+                    <form className="mb-4">
+                        <label htmlFor="passwordRange" className="block mb-2">Panjang <span className="text-blue-500 font-bold">{passwordLength}</span> karakter</label>
+                        <input type="range" id="passwordRange" className="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" min="8" value={passwordLength} onChange={handlePasswordLength} max="128"/>
+                    </form>
+
                     <div className="w-full">
                         <p className="mb-4">Hasil Generate</p>
-                        <div className="text-center border border-gray-300 rounded py-6 mb-5">
-                            <h4 className="text-2xl">jah8098321hjk</h4>
+                        <div className="text-center border border-gray-300 rounded py-6 px-4 mb-5 text-wrap">
+                            <h4 className="text-2xl w-full wrap-break-word">{ password }</h4>
                         </div>
                         <button onClick={handleGeneratePassword} className="w-full bg-blue-700 py-3 text-white rounded hover:bg-blue-800 transition duration-250 ease-in-out">Generate</button>
                     </div>
